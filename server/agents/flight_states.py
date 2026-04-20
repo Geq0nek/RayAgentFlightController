4c36ee3fe5bce7a8e8b41807eb235cf9ab4b4523
Geq0nek <tomaszdrag19@gmail.com>
2026-04-20 19:41:33 +0000

    Delete unused fligt states

--- server/agents/flight_states.py
+++ server/agents/flight_states.py
@@ -2,9 +2,5 @@ from enum import Enum, auto
 
 class FlightState(Enum):
     PARKED = auto()      
-    TAKING_OFF = auto()  
-    IN_FLIGHT = auto()   
-    LANDING = auto()     
+    IN_FLIGHT = auto()     
     ARRIVED = auto()     
-    CANCELLED = auto()   
-    DELAYED = auto()
